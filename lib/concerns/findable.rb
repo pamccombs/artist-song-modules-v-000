d42b655252cda04206fc1dbf778e00f09d53cd50
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-13 20:52:29 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,5 +3,9 @@ module Findable
     def self.find_by_name(name)
       @@songs.detect{|a| a.name == name}
     end
+    
+    def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  en
   
 end
