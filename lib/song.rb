406dc8088fd413df8ce4c4850ec304cfa0c474d8
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-13 20:48:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
 require 'pry'
+require_relative '../lib/concerns/memorable'
 
 class Song
+  
+  extend Memorable::ClassMethods
+  
   attr_accessor :name
   attr_reader :artist
 
@@ -18,17 +22,17 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
+  #def self.reset_all
+    #@@songs.clear
+  #end
 
   def to_param
     name.downcase.gsub(' ', '-')
   end
 
-  def self.count
-    self.all.count
-  end
+  #def self.count
+    #self.all.count
+  #end
 
   def self.all
     @@songs
